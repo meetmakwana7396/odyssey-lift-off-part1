f9dbc07ff889c167d28e6033ef01adc083a2f02e
meetmakwana7396 <makwnameet7301@gmail.com>
2024-07-21 12:09:48 +0530

    pending from mock, schema.

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,15 @@
-const {ApolloServer} = require("@apollo/server")
+const { ApolloServer } = require("@apollo/server");
+const { startStandaloneServer } = require("@apollo/server/standalone");
+const { addMocksToSchema } = require("@graphql-tools/mock");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
+const typeDefs = require("./schema");
+
+async function startApolloServer() {
+  const server = new ApolloServer({ schema });
+  const { url } = await startStandaloneServer(server);
+  console.log(`
+    🚀 Server is running!
+    🙋‍♂️ Query at ${url}
+    `);
+}
+startApolloServer();
